9566910ec30e5961aee680e97381be912c01b502
Pavel Taruts <ptrtss@gmail.com>
2022-05-23 20:04:51 +0300

    Extracting property files parsing in build scripts

---
+++ buildSrc/src/main/groovy/GitLabParameters.groovy
@@ -0,0 +1,34 @@
+import org.apache.commons.io.FileUtils
+
+class GitLabParameters {
+    URI gitlabUri
+    URI projectUri
+    String username
+    String password
+
+    static GitLabParameters getGitLabParameters(String propertiesResourcePath) {
+        String resourcesDir = project(':app').sourceSets.main.resources.srcDirs.first()
+        File localGitlabPropertiesFile = FileUtils.getFile(resourcesDir, propertiesResourcePath)
+        Properties localGitlabProperties = new Properties()
+        try (Reader reader = localGitlabPropertiesFile.newReader()) {
+            localGitlabProperties.load(reader)
+        }
+
+        GitLabParameters gitLabParameters = new GitLabParameters()
+        String propertiesProjectUrlStr = localGitlabProperties.getProperty('dynamic-impl.git-repository.url')
+
+        gitLabParameters.username = localGitlabProperties.getProperty('dynamic-impl.git-repository.username')
+        gitLabParameters.password = localGitlabProperties.getProperty('dynamic-impl.git-repository.password')
+
+        gitLabParameters.projectUri = new URI(propertiesProjectUrlStr)
+        gitLabParameters.gitlabUri = new URI(
+            gitLabParameters.projectUri.scheme,
+            gitLabParameters.projectUri.authority,
+            null,
+            null,
+            null
+        )
+
+        return gitLabParameters
+    }
+}
