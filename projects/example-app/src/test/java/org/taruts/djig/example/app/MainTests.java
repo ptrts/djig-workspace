3cbf5a6eed03fec3c633590c174dc0ff51f90ce1
Pavel Taruts <ptrtss@gmail.com>
2022-07-26 00:55:22 +0300

    todo

--- projects/example-app/src/test/java/org/taruts/djig/example/app/MainTests.java
+++ projects/example-app/src/test/java/org/taruts/djig/example/app/MainTests.java
@@ -10,6 +10,8 @@ class MainTests {
     // todo Тесты ругаются, т.к. @DynamicProjectQualifier("1") есть,
     //      а динамических проектов в свойствах тестов - нет.
     //      Надо бы как-то эту проблему решить
+    //      Наверное, проект в свойствах нужно оставить,
+    //      но в тестах отключить его выкачивание при старте приложения
     //@Test
     void contextLoads() {
         System.out.println("Module name = " + getClass().getModule().getName());
